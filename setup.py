054166a8f444502055b4be85b2087b342e39c91e
Gabriel Deo <gabthedeo@gmail.com>
2023-12-11 22:36:36 -0500

    setup + hessian computation

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='trade-prescription',
+    version='0.1.0',
+    description='Prediction/prescription framework for large trade orders',
+    url=''
+,    author='Tristan Magnin, Gabriel Deo',
+    author_email='',
+    license='',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'scipy',
+        'matplotlib',
+        'sympy',
+    ]
+)
